d92a5c9ed3f02c1dc7c796f05a3dcfb66e53cdd8
Ivan Trubach <mr.trubach@icloud.com>
2020-02-13 11:32:20 +0300

    Don’t use Config.LangKey annotation
    Forge has default lang keys for config localization.
    
    	<modid> ( <category> "." ) * <name>

--- src/main/java/unloader/UnloaderConfig.java
+++ src/main/java/unloader/UnloaderConfig.java
@@ -3,20 +3,17 @@ package unloader;
 import net.minecraftforge.common.config.Config;
 
 @Config(modid = UnloaderMod.MODID)
-@Config.LangKey(UnloaderMod.MODID + ".config.title")
 public class UnloaderConfig {
     @Config.Name("blacklistDims")
     @Config.Comment({
         "List of dimensions you don’t want to unload.",
         "Can be dimension name or ID. Uses regular expressions.",
     })
-    @Config.LangKey(UnloaderMod.MODID + ".config.blacklistDims")
     public static String[] blacklistDims = {"0", "overworld"};
 
     @Config.Name("unloadCheck")
     @Config.Comment({
         "Time (in ticks) to wait before checking dimensions",
     })
-    @Config.LangKey(UnloaderMod.MODID + ".config.unloadCheck")
     public static Integer unloadInterval = 600;
 }
