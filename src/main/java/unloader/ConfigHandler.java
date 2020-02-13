af93646ccccd019ed72e7e1cf4b3afa73da98a4a
Ivan Trubach <mr.trubach@icloud.com>
2020-02-13 11:38:46 +0300

    Fix typo

--- src/main/java/unloader/ConfigHandler.java
+++ src/main/java/unloader/ConfigHandler.java
@@ -1,17 +1,18 @@
 package unloader;
 
+import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod.EventBusSubscriber(modid = Unloader.MODID)
+@Mod.EventBusSubscriber(modid = UnloaderMod.MODID)
 public class ConfigHandler {
     @SubscribeEvent
     public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
         if (!event.getModID().equals(UnloaderMod.MODID)) {
             return;
         }
-        ConfigManager.sync(UnloaderMod.MODID, Type.INSTANCE);
+        ConfigManager.sync(UnloaderMod.MODID, Config.Type.INSTANCE);
     }
 }
